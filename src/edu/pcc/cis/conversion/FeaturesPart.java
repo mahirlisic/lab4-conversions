75aa90fca9ccd414da6945cd3337bbaa4d157183
Mahir <mahir.lisic@pcc.edu>
2023-03-21 13:55:46 -0700

    Added date

--- src/edu/pcc/cis/conversion/FeaturesPart.java
+++ src/edu/pcc/cis/conversion/FeaturesPart.java
@@ -1,7 +1,9 @@
 package edu.pcc.cis.conversion;
+import java.util.Date;
 
 public class FeaturesPart {
     public static void main(String[] args){
         System.out.println("Hello, world!");
+        System.out.println(new Date());
     }
 }
